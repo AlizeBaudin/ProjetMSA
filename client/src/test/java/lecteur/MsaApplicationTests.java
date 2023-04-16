5d7d17a4e04f81fa819370a758235d2483bc472d
Alizé Baudin <114283033+AlizeBaudin@users.noreply.github.com>
2023-04-16 15:51:48 +0200

    Lecteur

---
+++ client/src/test/java/lecteur/MsaApplicationTests.java
@@ -0,0 +1,13 @@
+package lecteur;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.Test;
+
+@SpringBootTest
+class MsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
